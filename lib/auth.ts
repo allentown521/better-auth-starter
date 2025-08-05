14730d22a3414b5212ee46cbb2c0f7f49de14c87
allentown <tangxiangpi@gmail.com>
2025-08-05 14:21:17 +0800

    feat(auth): add session cookie cache and VS Code config files
    https://www.better-auth.com/docs/concepts/session-management#session-caching

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,29 +3,35 @@ import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./prisma";
 
 export const auth = betterAuth({
-   database: prismaAdapter(prisma, {
-      provider: "postgresql"
-   }),
-   emailAndPassword: {
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    autoSignIn: false,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+  },
+  rateLimit: {
+    window: 60, // time window in seconds
+    max: 30, // max requests per window
+  },
+  session: {
+    cookieCache: {
       enabled: true,
-      autoSignIn: false
-   },
-   socialProviders: {
-      google: {
-         clientId: process.env.GOOGLE_CLIENT_ID as string,
-         clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-      },
-      github: {
-         clientId: process.env.GITHUB_CLIENT_ID as string,
-         clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-      }
-   },
-   rateLimit: {
-      window: 60, // time window in seconds
-      max: 30, // max requests per window
-   },
-   trustedOrigins: [
-      "chrome-extension://faapjophjjnpgikffnbjjiikppambglm",
-      "chrome-extension://cjaecgdalifbiajfmkkmiffdhcnckmih",
-   ]
-})
+      maxAge: 5 * 60, // Cache duration in seconds
+    },
+  },
+  trustedOrigins: [
+    "chrome-extension://faapjophjjnpgikffnbjjiikppambglm",
+    "chrome-extension://cjaecgdalifbiajfmkkmiffdhcnckmih",
+  ],
+});
